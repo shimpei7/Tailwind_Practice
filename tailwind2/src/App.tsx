c94d09a7084fd838c8011eda0adebf21cc64bba8
shimpei.w <shimpei.watanabe.7@gmail.com>
2022-06-22 21:49:25 +0900

    tailwind機能していることを確認

--- tailwind2/src/App.tsx
+++ tailwind2/src/App.tsx
@@ -7,6 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around item-center">
+        <p className="m-0 text-gray-400">Tailwind CSS です</p>
+            <button className="bg-gray-300 border-0 p-2 hover:bg-gray-400 hover:text-white">ボタン</button>
+            </div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
